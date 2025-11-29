0db8f287b84494dea3144a7dc1bef819992b35cf
springland <fenglinmail@yahoo.com>
2025-11-29 07:43:59 -0500

    add custom tracing for getProduct

--- spring-microservice-opentelemetry/tracing-cart-service/src/main/java/com/springland365/tracing/cart/service/CartService.java
+++ spring-microservice-opentelemetry/tracing-cart-service/src/main/java/com/springland365/tracing/cart/service/CartService.java
@@ -1,6 +1,8 @@
 package com.springland365.tracing.cart.service;
 
 import com.springland365.tracing.common.dto.Product;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,7 @@ public class CartService {
 
     protected final WebClient webClient ;
 
+    protected final ObservationRegistry  registry;
     @Observed(name = "user.name",
             contextualName = "cart-service.addItem",
             lowCardinalityKeyValues = {"userType", "userType2"})
@@ -27,11 +30,20 @@ public class CartService {
     public Product addItem(  Long  id){
 
         log.info(" add product {}" , id);
-        Product product = getProduct(id);
+        Product product = getProductObserved(id);
         return product ;
 
     }
 
+    protected Product getProductObserved(Long id){
+
+        return Observation.createNotStarted(
+                "cart-service.getProduct",
+                registry
+                ).lowCardinalityKeyValue("type" , "service")
+                .observe(() -> getProduct(id));
+
+    }
     protected Product getProduct(Long id){
         log.info(" get product {}" , id);
         Product product = webClient.get()
